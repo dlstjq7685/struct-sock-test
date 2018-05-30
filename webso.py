427a152e9188578bc1b86558e6b441b3c89ae27a
dlstjq7685 <dlstjq7685@gmail.com>
2018-05-30 09:52:09 +0900

    update

---
+++ webso.py
@@ -0,0 +1,11 @@
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+
+
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 8001))
+asyncio.get_event_loop().run_forever()
